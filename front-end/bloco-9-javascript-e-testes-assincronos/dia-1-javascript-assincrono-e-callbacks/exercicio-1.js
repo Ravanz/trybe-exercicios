e0ab483e89d74f80b56f8495f11b92e793f0d354
Ravanz <rayteon.h@gmail.com>
2022-05-31 18:59:56 -0300

    criando função sendMarsTemperature

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio-1.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio-1.js
@@ -1,14 +1,29 @@
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  console.log("Returned planet: ", mars);
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   console.log("Returned planet: ", mars);
+// };
+
+// setTimeout(() => {
+//   getPlanet();
+// }, 4000);  // imprime Marte depois de 4 segundos
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
 };
 
-setTimeout(() => {
-  getPlanet();
-}, 4000);  // imprime Marte depois de 4 segundos
+const sendMarsTemperature = () => {
+  const marsTemp = getMarsTemperature();
+  console.log(`Marns termperature is: ${marsTemp} `)
+}
+// crie a função sendMarsTemperature abaixo
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
