82fe3861de8df507f7eb826435d9fbe481305af9
Ravanz <rayteon.h@gmail.com>
2022-07-22 15:09:07 -0300

    Exercícios dia 15.1

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
@@ -1,1 +1,38 @@
-const Redux = require('redux')
+const Redux = require('redux');
+// ========= ACTION TYPE ==========
+
+
+// ========= ACTION ==========
+const actionNextColor = () => {
+  store.dispatch({type: NEXT_COLOR})
+}
+const actionPreviousColor = () => {
+  store.dispatch({type: PREVIOUS_COLOR})
+}
+
+// ========= REDUCER ==========
+const INITIAL_STATE = {
+  index: 0
+}
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length -1 ? 0 : state.index + 1
+      }
+    case 'PREVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length : state.index - 1
+      }
+    default:
+      return INITIAL_STATE
+  }
+}
+
+// ======== STORE =========
+const store = Redux.createStore(reducer);
+
+
+
